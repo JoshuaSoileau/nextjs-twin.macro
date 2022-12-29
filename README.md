7201ab647490066b7f8136c76c373f02548b872c
Josh Soileau <joshuasoileau@fb.com>
2022-12-29 15:33:46 -0500

    Initial commit. Sets up nextjs w/twin.macro.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 This is a starter template for [Learn Next.js](https://nextjs.org/learn).
+
+[This](https://github.com/ben-rogerson/twin.macro/blob/master/docs/prop-styling-guide.md) is a guide on using wtin.macro for normal jsx element styling.
+[This](https://github.com/ben-rogerson/twin.macro/blob/master/docs/styled-component-guide.md) is a guide on how to use twin.macro with styled components for styling.
--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,41 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css';
+import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <h1 className="title">
+          Read <Link href="/posts/first-post">this page!</Link>
         </h1>
 
-        <p className={styles.description}>
+        <p>
           Get started by editing <code>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+        <div>
+          <a href="https://nextjs.org/docs">
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <a href="https://nextjs.org/learn">
             <h3>Learn &rarr;</h3>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
+          <a href="https://github.com/vercel/next.js/tree/master/examples">
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
+          <a href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
@@ -56,7 +51,12 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
+          <Image
+            src="/profile.jpg" // Route of the image file
+            height={144} // Desired size with correct aspect ratio
+            width={144} // Desired size with correct aspect ratio
+            alt="Your Name"
+          />
         </a>
       </footer>
 
